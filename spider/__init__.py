4a4eccf57cb20e19e922474988fdcf311708669a
huabusi <huabusi@pinduoduo.com>
2020-05-07 18:58:14 +0800

    msg

--- spider/__init__.py
+++
@@ -1,3 +0,0 @@
-#author Frank_Lee
-# email=lizhipengqilu@gmail.com
-#2019/3/21
--- spider/spiders/__init__.py
+++
@@ -1,7 +0,0 @@
-#author Frank_Lee
-# email=lizhipengqilu@gmail.com
-#2019/3/21
-# This package will contain the spiders of your Scrapy project
-#
-# Please refer to the documentation for information on how to create and manage
-# your spiders.
